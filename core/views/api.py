a7991e1963aab477c404b0e8df4c4f66955e97ad
Mike Kanning <mikekanning@gmail.com>
2017-03-02 12:38:15 -0500

    add slug to type api output

--- core/views/api.py
+++ core/views/api.py
@@ -2,14 +2,14 @@ import json
 from django.shortcuts import HttpResponse
 from chronam.core.models import FundingSource, Type
 
-def newspaper_types():
+def newspaper_types(request):
     data = []
     for t in Type.objects.all():
-        type_data = dict(id = t.id, name = t.name)
+        type_data = dict(id = t.id, slug = t.slug, name = t.name)
         data.append(type_data)
     return HttpResponse(json.dumps(data), content_type='application/json')
 
-def funding_sources():
+def funding_sources(request):
     data = []
     for s in FundingSource.objects.all():
         source_data = dict(id = s.id, slug = s.slug, name = s.name)
