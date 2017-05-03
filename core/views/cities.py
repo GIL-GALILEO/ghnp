80ef3e8967126cf8c55e65dabb37184d6497d664
Mike Kanning <mikekanning@gmail.com>
2017-05-03 17:44:46 -0400

    bugfix and refactor of cities view. closes #93

--- core/views/cities.py
+++ core/views/cities.py
@@ -4,32 +4,23 @@ from django.template import RequestContext
 from chronam.core.models import Place, Title
 
 def cities_page(request):
-
     page_title = 'Browse by City'
     crumbs = list(settings.BASE_CRUMBS)
     crumbs.extend([{
         'label': page_title,
     }])
-
-    cities_with_titles = Place.objects.filter(titles__in=Title.objects.all).values('city')
+    cities_with_titles = Place.objects.filter(titles__in=Title.objects.all).filter(city__isnull=False).distinct().values_list('city', flat=True)
     cities_with_titles_by_letter = {}
-
     for c in cities_with_titles:
-
-        city_name = c['city']
-
         city_titles = {
-            'city': city_name,
-            'titles': Title.objects.filter(places__city=city_name)
+            'city': c,
+            'titles': Title.objects.filter(places__city=c)
         }
-
-        first_letter = city_name[:1]
-
+        first_letter = c[:1]
         if first_letter in cities_with_titles_by_letter:
             cities_with_titles_by_letter[first_letter].append(city_titles)
         else:
             cities_with_titles_by_letter[first_letter] = [city_titles]
-
     return render_to_response('cities.html',
                               dictionary=locals(),
                               context_instance=RequestContext(request))
