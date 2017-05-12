ebaaf886e9add8c671388cfaea529d1faa564444
Mike Kanning <mikekanning@gmail.com>
2017-05-12 13:58:16 -0400

    fix thumbnail result highlighting. closes #106. closes #104.

--- core/static/js/highlight.js
+++ core/static/js/highlight.js
@@ -1,34 +1,31 @@
 (function($) {
 
     function add_highlights(image) {
-        if (image.data('highlighted')==true) {
+        if (image.data('highlighted') == true) {
             return
         }
         var width = image.width();
         var height = image.height();    
-        if (width>0 && height>0) {
-            image.data('highlighted', true)
-            var script_name = image.data('script_name');
+        if (width > 0 && height > 0) {
+            image.data('highlighted', true);
             var id = image.data('id');
             var words = image.data('words');
-            $.getJSON(script_name+id+'coordinates/', function(all_coordinates) {
+            var url = id+'coordinates/';
+            $.getJSON(url, function(all_coordinates) {
                 image.wrap('<div class="highlight_words" style="display: inline-block; position: relative; margin: 0px; padding: 0px; height: auto; width: auto;" />');
-                var div = image.parents("div.highlight_words")
-		div.wrap('<div style="text-align: center" />')
-
+                var div = image.parents("div.highlight_words");
+		        div.wrap('<div style="text-align: center" />');
                 var vScale = 100 / all_coordinates["height"];
                 var hScale = 100 / all_coordinates["width"];
                 $.each(words.split(" "), function(index, word) {
-                    var boxes = [];
                     var coordinates = all_coordinates["coords"][word];
-                    for (k in coordinates) {
+                    for (var k in coordinates) {
                         var v = coordinates[k];
-                        div.append("<div style='position: absolute; " +
-    "TOP: " +  (v[1] * vScale) + '%; ' +
-    "LEFT: " +       (v[0] * hScale) + '%; ' +
-    "HEIGHT: " +     (v[3] * vScale) + '%; ' +
-    "WIDTH: " +      (v[2] * hScale) + "%;'/>");
-
+                        div.append("<div class='overlay' style='position: absolute; " +
+                            'top: ' + (v[1] * vScale) + '%; ' +
+                            'left: ' + (v[0] * hScale) + '%; ' +
+                            'height: ' + (v[3] * vScale) + '%; ' +
+                            'width: ' + (v[2] * hScale) + "%;' />");
                     }
                 });
             });
@@ -36,16 +33,17 @@
     }
 
     function init() {
-        $("img.highlight_words").load(function(i) {
+        var highlight_words = $("img.highlight_words");
+        highlight_words.load(function() {
             add_highlights($(this));
         });
-        $("img.highlight_words").each(function(i) {
+        highlight_words.each(function() {
             if (this.complete) {
-		add_highlights($(this));
-	    }
-	});
-    };
+		        add_highlights($(this));
+	        }
+	    });
+    }
 
     $(init);
 
-})(jQuery)
+})(jQuery);
