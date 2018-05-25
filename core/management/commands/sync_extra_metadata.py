da46e59462736c1aea0436be1ab95ba5b7784593
Mike Kanning <mikekanning@gmail.com>
2018-05-25 15:37:35 -0400

    update additional metadata job to use new JSON format which moves the lccns to be updated inside the JSON. modify django management task to read new format.

---
+++ core/management/commands/sync_extra_metadata.py
@@ -0,0 +1,77 @@
+import sys
+import simplejson as json
+import os
+
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+from chronam.core.models import Title, FundingSource, NewspaperType
+from django.db.utils import IntegrityError
+
+class Command(BaseCommand):
+    help = 'Adds custom GHNP metadata to title records by LCCN'
+
+    def handle(self, *args, **options):
+
+        files_processed = 0
+        json_path = settings.EXTRA_METADATA
+
+        # iterate through each file
+        if not os.path.isdir(json_path):
+            raise CommandError('EXTRA_METADATA path in settings.py is not a directory!')
+
+        json_dir = os.listdir(json_path)
+
+        for json_file in json_dir:
+            if os.path.isdir(json_file):
+                continue
+            # get data from JSON file
+            try:
+
+                with open(os.path.join(json_path, json_file), 'rb') as f:
+                    data = json.load(f)
+                lccns = data.get('lccn')
+                funding_source_code = data.get('funding_source').strip()
+                newspaper_type_codes = data.get('newspaper_types')
+                essay_text = data.get('essay').strip()
+            except IOError, _:
+                raise CommandError('Problem reading JSON file at %s' % options['json_path'])
+            except json.JSONDecodeError, e:
+                raise CommandError('JSON parsing error: %s' % e)
+
+            # get titles from lccns
+            try:
+                titles = [Title.objects.get(lccn=lccn) for lccn in lccns]
+            except:
+                raise CommandError('At least one of your LCCNs specified in %s could not be found' % json_file)
+
+            # get NewspaperType objects
+            try:
+                newspaper_types = [NewspaperType.objects.get(slug=code) for code in newspaper_type_codes]
+            except:
+                raise CommandError('Newspaper Type "%s" could not be found' % code)
+
+            # get FundingSource object
+            if funding_source_code:
+                try:
+                    funding_source = FundingSource.objects.get(slug=funding_source_code)
+                except:
+                    raise CommandError('Funding Source "%s" could not be found' % funding_source_code)
+
+            # apply to titles
+            for title in titles:
+                try:
+                    if newspaper_types: title.newspaper_types = newspaper_types
+                    if funding_source_code: title.funding_source = funding_source
+                    if essay_text: title.essay_text = essay_text
+                    title.save()
+                except IntegrityError as e:
+                    raise CommandError('Problem saving title with lccn %s: %s' % (title.lccn, format(e.errno, e.strerror)))
+                except:
+                    e = sys.exc_info()[0]
+                    raise CommandError('Problem saving title with lccn %s: %s' % (title.lccn, e))
+
+            self.stdout.write('Sync complete for file %s. %i titles updated.' % json_file, len(titles))
+
+            files_processed += 1
+
+        self.stdout.write('JSON files synced: %i' % files_processed)
